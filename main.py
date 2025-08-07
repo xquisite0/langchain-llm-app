fdb5df54cefb2b849efbda1bd9cc95aeca840919
Zhou Yuhang <zyuhang255@gmail.com>
2025-08-07 09:40:41 +0800

    integrate llm feature into web app

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ import langchain_helper as lch
 import streamlit as st
 
 st.title("Pets name generator")
+
+user_animal_type = st.sidebar.selectbox("What is your pet?", ("Cat", "Dog", "Cow", "Hamster"))
+
+# limit max chars to prevent excessive input for openAI API 
+user_pet_color = st.sidebar.text_area(label=f"What color is your {user_animal_type}?", max_chars=15)
+
+if user_pet_color:
+    response = lch.generate_pet_name(user_animal_type, user_pet_color)
+    print(response)
+    st.text(response)
