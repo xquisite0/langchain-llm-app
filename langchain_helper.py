d41a56a2543c4a8cf99ddcf461f5f1807a082bd5
Zhou Yuhang <zyuhang255@gmail.com>
2025-08-07 09:17:09 +0800

    reorder file structure

---
+++ langchain_helper.py
@@ -0,0 +1,24 @@
+from langchain_community.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+from langchain.chains import LLMChain
+
+load_dotenv()
+
+def generate_pet_name(animal_type, pet_color):
+    # temperature: 0 safe outputs, 1 creative outputs with risk of hallucination
+    llm = OpenAI(temperature=0.7)
+
+    prompt_template_name = PromptTemplate(
+        input_variables=['animal_type', 'pet_color'],
+        template="I have a {animal_type} pet and I want a cool name for it. It is {pet_color} in color. Suggest me five cool names for my pet."
+    )
+
+    # chains the llm & prompt template together
+    name_chain = LLMChain(llm=llm, prompt=prompt_template_name)
+    
+    response = name_chain({'animal_type': animal_type, 'pet_color': pet_color})
+    return response
+
+if __name__ == "__main__":
+    print(generate_pet_name("cat", "black"))
